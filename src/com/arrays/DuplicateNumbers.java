c16acf7db3a2391921b94f3bb691747f082b82c6
ravi-kumar-24 <hiravi2410@gmail.com>
2020-06-07 10:43:03 +0530

    Initial commit

---
+++ src/com/arrays/DuplicateNumbers.java
@@ -0,0 +1,17 @@
+package com.arrays;
+
+public class DuplicateNumbers {
+    public static void main(String[] args) {
+        int[] arr = {4,5,5,5,5,6,2,4,1,1,4,4,6,7,9,8,6,4,3,3,2,2,7,3,5,6,7,8,8,9,4,7,1,8};
+        int[] temp = new int[arr.length];
+        for (int i = 0; i <arr.length; i++) {
+            temp[arr[i]]++;
+        }
+
+        for (int i=0; i<temp.length;i++) {
+            if(temp[i]>0) {
+                System.out.println(i +" -> " +temp[i]);
+            }
+        }
+    }
+}
