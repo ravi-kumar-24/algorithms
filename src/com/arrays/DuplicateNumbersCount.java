6435eda84dd499ecd4b05b7865861da7c5678b20
ravi-kumar-24 <hiravi2410@gmail.com>
2020-06-07 10:54:19 +0530

    updated name

--- src/com/arrays/DuplicateNumbersCount.java
+++ src/com/arrays/DuplicateNumbersCount.java
@@ -1,6 +1,6 @@
 package com.arrays;
 
-public class DuplicateNumbers {
+public class DuplicateNumbersCount {
     public static void main(String[] args) {
         int[] arr = {4,5,5,5,5,6,2,4,1,1,4,4,6,7,9,8,6,4,3,3,2,2,7,3,5,6,7,8,8,9,4,7,1,8};
         int[] temp = new int[arr.length];
